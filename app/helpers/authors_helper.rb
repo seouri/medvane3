5dd5e363eab35846f55b7925f987aea4c8507680
Joon Lee <seouri@gmail.com>
2010-01-19 13:40:35 -0500

    added 'see also' for author

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,12 @@
 module AuthorsHelper
+  def see_also(author)
+    return if author.see_also.size == 0
+    period = @period == "all" ? nil : @period
+    li = []
+    author.see_also.each do |see|
+      li.push(content_tag(:li, link_to(see.to_l, bibliome_author_path(@bibliome, see, :period => period) + "/#{period}")))
+    end
+    ul = content_tag :ul, li.join("\n")
+    content_tag :div,"See also: " + ul, :id => "see_also"
+  end
 end
