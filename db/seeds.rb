dfdc88c7f5799adc606716531150b3275af9d470
Joon Lee <seouri@gmail.com>
2010-01-28 17:24:21 -0500

    show executed time for rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 descriptor_file = File.join(File.dirname(__FILE__), 'desc.2010.txt')
 if File.stat(descriptor_file).size > 0
+  puts "[#{Time.now.to_s}] load Subject"
   File.foreach(descriptor_file) do |record|
     s = Subject.new
     s.id, s.term, trees = record.chomp.split("\t")
@@ -9,6 +10,7 @@ end
 
 mesh_trees_file = File.join(File.dirname(__FILE__), 'mesh_trees.2010.txt')
 if File.stat(mesh_trees_file).size > 0
+  puts "[#{Time.now.to_s}] load MeshTree"
   File.foreach(mesh_trees_file) do |record|
     t = MeshTree.new
     t.id, t.tree_number, t.subject_id, t.parent_id = record.chomp.split("\t")
@@ -24,6 +26,7 @@ end
 
 mesh_ancestors_file = File.join(File.dirname(__FILE__), 'mesh_ancestors.2010.txt')
 if File.stat(mesh_ancestors_file).size > 0
+  puts "[#{Time.now.to_s}] load MeshAncestor"
   File.foreach(mesh_ancestors_file) do |record|
     t = MeshAncestor.new
     t.subject_id, t.ancestor_id = record.chomp.split("\t")
